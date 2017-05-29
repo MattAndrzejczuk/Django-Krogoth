a4e0c35b2f117b2b0c79d7e201e6dcd0b0e68216
Matt Andrzejczuk <cinicraft@Matts-iMac.local>
2017-05-28 17:14:04 -0700

    static files.

--- jawn/settings.py
+++ jawn/settings.py
@@ -154,7 +154,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
 
-    #os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static'),
     #'/Users/Dominooch/Dropbox/Dev/theEmpire/static/',
 )
 
