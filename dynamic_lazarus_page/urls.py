56233083cdf2c923e42dd72dd1f0fa1c69e3176b
Ubuntu <ubuntu@ip-172-31-19-254.us-west-2.compute.internal>
2017-07-08 09:44:22 +0000

    new version and features

--- dynamic_lazarus_page/urls.py
+++ dynamic_lazarus_page/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from dynamic_lazarus_page import views
 
 from dynamic_lazarus_page.views import SampleAPI, CustomHtmlGenerator, DynamicIndexModule, SuperBasicModelView, \
-    AngularFuseApplicationView, FuseAppComponentView, DynamicJavaScriptInjector, DynamicHTMLInjector
+    AngularFuseApplicationView, FuseAppComponentView, DynamicJavaScriptInjector, DynamicHTMLInjector, \
+    DynamicIndexRoute, DynamicHTMLToolbar
 
 
 
@@ -10,6 +11,8 @@ urlpatterns = [
     url(r'^sample_api/', SampleAPI.as_view(), name='sample API'),
     url(r'^sample_html/', CustomHtmlGenerator.as_view(), name='sample HTML'),
     url(r'^DynamicIndexModule/', DynamicIndexModule.as_view(), name='DynamicIndexModule'),
+    url(r'^DynamicIndexRoute/', DynamicIndexRoute.as_view(), name='DynamicIndexRoute'),
+    url(r'^DynamicHTMLToolbar/', DynamicHTMLToolbar.as_view(), name='DynamicHTMLToolbar'),
 
     url(r'^DynamicJavaScriptInjector/', DynamicJavaScriptInjector.as_view(), name='DynamicJavaScriptInjector'),
     url(r'^DynamicHTMLInjector/', DynamicHTMLInjector.as_view(), name='DynamicHTMLInjector'),
