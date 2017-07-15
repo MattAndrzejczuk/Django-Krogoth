ee1c1968798470377438d8bda87e136eb30559a5
Ubuntu <ubuntu@ip-172-31-19-254.us-west-2.compute.internal>
2017-07-15 15:17:25 +0000

    added theme constants partials

--- DjangularStaticFiles/fuse-themes.constant.pt1.js
+++ DjangularStaticFiles/fuse-themes.constant.pt1.js
@@ -2,11 +2,5 @@
 {
     'use strict';
 
-    /// ARM THEME:
-    // "ArmTheme" : {"primary":{"name":"blue","hues":{"default":"900","hue-1":"A700","hue-2":"700","hue-3":"800"}},"accent":{"name":"green","hues":{"default":"800","hue-1":"700","hue-2":"A700","hue-3":"A400"}},"warn":{"name":"deep-orange","hues":{"default":"A400","hue-1":"700","hue-2":"500","hue-3":"A400"}},"background":{"name":"fuse-paleblue","hues":{"default":"900","hue-1":"700","hue-2":"500","hue-3":"600"}}}
-    // "ArmTheme2" : {"primary":{"name":"fuse-blue","hues":{"default":"900","hue-1":"A700","hue-2":"300","hue-3":"A100"}},"accent":{"name":"teal","hues":{"default":"900","hue-1":"A700","hue-2":"300","hue-3":"A100"}},"warn":{"name":"deep-orange","hues":{"default":"A700","hue-1":"900","hue-2":"500","hue-3":"A100"}},"background":{"name":"fuse-paleblue","hues":{"default":"900","hue-1":"600","hue-2":"800","hue-3":"A700"}}}
-    // "ArmTheme3" : {"primary":{"name":"fuse-blue","hues":{"default":"900","hue-1":"A700","hue-2":"A100","hue-3":"200"}},"accent":{"name":"lime","hues":{"default":"900","hue-1":"A700","hue-2":"A400","hue-3":"300"}},"warn":{"name":"red","hues":{"default":"A700","hue-1":"900","hue-2":"500","hue-3":"A100"}},"background":{"name":"fuse-paleblue","hues":{"default":"900","hue-1":"A700","hue-2":"100","hue-3":"300"}}}
-    /// CORE THEME:
-    // "CUSTOM_THEME_NAME" : {"primary":{"name":"deep-purple","hues":{"default":"900","hue-1":"A700","hue-2":"300","hue-3":"A100"}},"accent":{"name":"red","hues":{"default":"900","hue-1":"A700","hue-2":"300","hue-3":"A100"}},"warn":{"name":"amber","hues":{"default":"A700","hue-1":"900","hue-2":"300","hue-3":"A100"}},"background":{"name":"fuse-paleblue","hues":{"default":"900","hue-1":"600","hue-2":"800","hue-3":"A700"}}}
 
     var fuseThemes = {
