275c418b3c381c2939d5760fb27d6d7ce849d9b6
Matt Andrzejczuk <Matt@jawn.it>
2017-07-28 01:28:03 -0400

    the default page in the js should now be 'home' instead of KMD

--- rest_auth/views.py
+++ rest_auth/views.py
@@ -163,6 +163,7 @@ def index(request):
 
 
 
+
     DjangularMasterViewControllers = []
     all_applications = DjangularMasterViewController.objects.all()
     for application in all_applications:
@@ -188,8 +189,23 @@ def index(request):
     index_route_js = '/static/app/index.route.js'
     try:
         mainHtmlLayout = NgIncludedHtml.objects.get(name='mainHtmlLayout')
-        index_route_js = mainHtmlLayout = mainHtmlLayout.url_helper
+        index_route_js = mainHtmlLayout
     except:
+        _main_ = '/static/app/core/layouts/vertical-navigation-fullwidth-toolbar-2.html'
+        _toolbar_ = '/static/app/toolbar/layouts/vertical-navigation-fullwidth-toolbar-2/toolbar.html'
+        _navigation_ = '/static/app/navigation/layouts/vertical-navigation-fullwidth-toolbar-2/navigation.html'
+        js_raw = "(function () {'use strict';angular.module('fuse').config(routeConfig); function routeConfig($stateProvider, $urlRouterProvider, $locationProvider) {$locationProvider.hashPrefix('!');$urlRouterProvider.otherwise('/home');var $cookies;angular.injector(['ngCookies']).invoke(['$cookies', function (_$cookies) {$cookies = _$cookies;}]);var layoutStyle = $cookies.get('layoutStyle') || 'LAYOUT_STYLE';' + " \
+                 "'var layouts = {LAYOUT_STYLE: {main: " + \
+                 _main_ + \
+                 ",toolbar: " + \
+                 _toolbar_ + \
+                 ",navigation: " + \
+                 _navigation_ + \
+                 "}, " + \
+                 "contentOnly: {main: '/static/app/core/layouts/content-only.html', toolbar: '', navigation: ''},contentWithToolbar: {main: '/static/app/core/layouts/content-with-toolbar.html',toolbar: '/static/app/toolbar/layouts/content-with-toolbar/toolbar.html',navigation: ''}};$stateProvider.state('app', {abstract: true,views: {'main@': {templateUrl: layouts[layoutStyle].main, controller: 'MainController as vm'},'toolbar@app': {templateUrl: layouts[layoutStyle].toolbar, controller: 'ToolbarController as vm'},'navigation@app': {templateUrl: layouts[layoutStyle].navigation,controller: 'NavigationController as vm'},'quickPanel@app': {templateUrl: '/static/app/quick-panel/quick-panel.html',controller: 'QuickPanelController as vm'}}});}})();"
+        newLayout = NgIncludedHtml(name='mainHtmlLayout', contents=js_raw)
+        newLayout.save()
+        index_route_js = newLayout.url_helper
         print(index_route_js)
 
     context = {
