5f8342757939ccb57f56bf230d82a7604d69ed48
Matt Andrzejczuk <Matt@jawn.it>
2017-08-02 11:09:35 -0400

    Set the website title to 'Djangular' instead of 'TA Lazarus'

--- rest_auth/views.py
+++ rest_auth/views.py
@@ -214,7 +214,7 @@ def index(request):
         print(' ☭ ☭ ☭ ☭ ☭ ☭ ☭ ☭ ☭ ☭ ☭ ')
 
     context = {
-        "message": "TA Lazarus " + current_build_1[:3] + "." + current_build_2,
+        "message": "Djangular " + current_build_1[:3] + "." + current_build_2,
         "DjangularMasterViewControllers": DjangularMasterViewControllers,
         "splash_title": splash_title,
         "font_size": font_size,
