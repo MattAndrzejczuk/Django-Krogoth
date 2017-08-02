fef8fedd524cbe50244c874f3200f42e048534c6
Matt Andrzejczuk <Matt@jawn.it>
2017-08-02 10:48:31 -0400

    testing our custom django-admin command

--- Djangular/management/commands/builddefaults.py
+++ Djangular/management/commands/builddefaults.py
@@ -9,11 +9,10 @@ class Command(BaseCommand):
     help = 'prints all Djangular Master View Controllers to allow for backups'
 
     def add_arguments(self, parser):
-        parser.add_argument('mvc_id', nargs="+", type="int")
+        parser.add_argument('mvc_id', nargs="+", type=int)
 
     def handle(self, *args, **options):
         for mvc_id in options['mvc_id']:
             djangularApp = DjangularMasterViewController.objects.get(id=mvc_id)
 
             self.stdout.write(self.style.SUCCESS(djangularApp.controller_js))
-            
