ef8f30fe3b00c092edd0dfa04f899e2148344872
Matt Andrzejczuk <Matt@jawn.it>
2017-08-02 10:37:06 -0400

    deleted unused directory

--- DjangularStaticFiles/newsController.js
+++
@@ -1,8 +0,0 @@
-(function () {
-    'use strict';
-    angular.module('app.FUSE_APP_NAME').controller('FUSE_APP_NAMEController', FUSE_APP_NAMEController);
-    function FUSE_APP_NAMEController() {
-        var vm = this;
-        vm.helloText = 'Welcome to FUSE_APP_NAME';
-    }
-})();
