ae2791cb59373256a00c07ae34d12aa4787ba6e4
Matt Andrzejczuk <Matt@jawn.it>
2017-08-03 16:12:48 -0400

    added built in analytics analizer view with a url

--- DatabaseSandbox/urls.py
+++ DatabaseSandbox/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from DatabaseSandbox.views import BasicUploadExample, UserAgentTracker, UploadDataTA
+from DatabaseSandbox.views import BasicUploadExample, UserAgentTracker, UploadDataTA, KubaNetAnalytics
 
 
 # UploadDataTA
