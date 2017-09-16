deadd024a1dbd121def8b905f3105fadee846e60
Ubuntu <ubuntu@ip-172-31-19-254.us-west-2.compute.internal>
2017-09-16 01:53:04 +0000

    updates made to copy for generic Djangular apps

--- Djangular/management/default_templates/register/controller.js
+++ Djangular/management/default_templates/register/controller.js
@@ -4,11 +4,11 @@
 
     function FUSE_APP_NAMEController($log, $http, $mdToast, $cookies, $state, $mdDialog) {
         var vm = this;
-        vm.name = 'Arm Prime';
+        vm.name = 'Djangular';
         vm.form = {};
 
         var defaultTabName = document.getElementById("DjangularMetaText_01").innerHTML;
-        document.getElementById("DjangularMetaText_01").innerHTML = 'ArmPrime Registration - (' + defaultTabName.replace('Djangular', '') + ')';
+        document.getElementById("DjangularMetaText_01").innerHTML = 'Djangular Registration - (' + defaultTabName.replace('Djangular', '') + ')';
 
         var eulaText = "http://slipsum.com/ The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee. Your bones don't break, mine do. That's clear. Your cells react to bacteria and viruses differently than mine. You don't get sick, I do. That's also clear. But for some reason, you and I react the exact same way to water. We swallow it too fast, we choke. We get some in our lungs, we drown. However unreal it may seem, we are connected, you and I. We're on the same curve, just on opposite ends. Normally, both your asses would be dead as fucking fried chicken, but you happen to pull this shit while I'm in a transitional period so I don't wanna kill you, I wanna help you. But I can't give you this case, it don't belong to me. Besides, I've already been through too much shit this morning over this case to hand it over to your dumb ass. ";
 
