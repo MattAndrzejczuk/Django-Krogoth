77abf7544baaf92b716f1fed1cf8dcf104f64ebe
Ubuntu <ubuntu@ip-172-31-19-254.us-west-2.compute.internal>
2017-11-26 20:23:01 +0000

    Added LazarusV models and serializers.

---
+++ LazarusV/models.py
@@ -0,0 +1,69 @@
+from django.db import models
+from polymorphic.models import PolymorphicModel
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import HStoreField
+from LazarusIV.models_tdf import CavedogBase, ModProject, LazarusBase
+# Create your models here.
+from LazarusIV.models import JawnUser
+
+#    V  -  5
+
+
+class ModPublication(models.Model):
+    title = models.CharField(max_length=50, default='Untitled Mod Publication')
+    description = models.TextField(max_length=1000)
+    requirements = models.TextField(max_length=1000)
+    support = models.TextField(max_length=1000)
+    website_url = models.CharField(max_length=150, null=True, blank=True)
+    total_minor_milestones = models.IntegerField(null=True, blank=True, default=1)
+    total_major_milestones = models.IntegerField(null=True, blank=True, default=0)
+    DISTRIBUTIONS = (
+        ('alpha', 'Alpha'),
+        ('beta', 'Beta'),
+        ('rc', 'Release Candidate'),
+        ('release', 'Release'),
+    )
+    kind = models.TextField(max_length=25, choices=DISTRIBUTIONS)
+    is_private = models.BooleanField(default=True)
+
+class ModBuild(models.Model):
+    rated_by = models.ForeignKey(ModPublication, on_delete=models.CASCADE, related_name='distribution')
+    v_major = models.IntegerField(default=0)
+    v_minor = models.IntegerField(default=1)
+    download_url = models.CharField(max_length=150)
+
+
+
+class WargamePackage(models.Model):
+    parent_proj = models.ForeignKey(ModProject, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    thumbnail_url = models.CharField(max_length=100)
+
+class WargameFile(models.Model):
+    pack = models.ForeignKey(WargamePackage, on_delete=models.CASCADE)
+    path = models.CharField(max_length=100)
+    unique_name = models.CharField(max_length=50)
+    kind = models.CharField(max_length=50)
+    thumbnail_url = models.CharField(max_length=100)
+
+class WargameData(models.Model):
+    pack = models.ForeignKey(WargamePackage, on_delete=models.CASCADE)
+    source = models.ForeignKey(LazarusBase, on_delete=models.CASCADE)
+    unique_name = models.CharField(max_length=50)
+    kind = models.CharField(max_length=50)
+    thumbnail_url = models.CharField(max_length=100)
+
+class UserRating(PolymorphicModel):
+    rated_by = models.ForeignKey(JawnUser, on_delete=models.CASCADE, related_name='vote_caster')
+    total_votes = models.IntegerField(default=0)
+
+class RatingCavedogBase(UserRating):
+    cavedog_base_tdf = models.ForeignKey(CavedogBase, on_delete=models.CASCADE, related_name='cavedog_tdf')
+    vote_value = models.IntegerField(default=1)
+
+class RatingModPublication(UserRating):
+    lazarus_published_mod = models.ForeignKey(ModPublication, on_delete=models.CASCADE, related_name='lazarus_mod_distribution')
+    vote_value = models.IntegerField(default=1)
+
+# RatingCavedogBase
+# RatingModPublication
---
+++ LazarusV/serializers.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers, exceptions
+from chat.models import JawnUser
+from LazarusV.models import ModPublication, ModBuild, WargamePackage, WargameFile, WargameData, \
+    UserRating, RatingCavedogBase, RatingModPublication
+#    V  -  5
+
+class ModPublicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ModPublication
+        fields = ('id', )
+
+class ModBuildSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ModBuild
+        fields = ('id', )
+
+class WargamePackageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WargamePackage
+        fields = ('id', )
+
+class WargameFileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WargameFile
+        fields = ('id', )
+
+class WargameDataSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WargameData
+        fields = ('id', )
+
+class UserRatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserRating
+        fields = ('id', )
+
+class RatingCavedogBaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RatingCavedogBase
+        fields = ('id', )
+
+class RatingModPublicationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RatingModPublication
+        fields = ('id', )
