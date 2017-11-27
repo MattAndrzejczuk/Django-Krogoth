bb3ca6d7dc64bd5b7e1d177b25e8f90b8b7ab709
Ubuntu <ubuntu@ip-172-31-19-254.us-west-2.compute.internal>
2017-11-27 22:15:20 +0000

    created serializers for models_tdf in LazarusIV

--- LazarusIV/serializers.py
+++ LazarusIV/serializers.py
@@ -32,3 +32,44 @@ class NotificationItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = NotificationItem
         fields = ('id', )
+
+# models_tdf:
+class ModProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ModProject
+        fields = ('id', )
+
+class CavedogBaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CavedogBase
+        fields = ('id', )
+
+class LazarusBaseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LazarusBase
+        fields = ('id', )
+
+class DamageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Damage
+        fields = ('id', )
+
+class LazarusWeaponTDFSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LazarusWeaponTDF
+        fields = ('id', )
+
+class LazarusFeatureTDFSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LazarusFeatureTDF
+        fields = ('id', )
+
+class LazarusDownloadTDFSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LazarusDownloadTDF
+        fields = ('id', )
+
+class LazarusUnitFBISerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LazarusUnitFBI
+        fields = ('id', )
