83e1fabeeedc7b92542920087b196aedae71cd88
cinicraft <matt@jawn.it>
2018-01-15 11:24:52 -0500

    Three ultra editor services are finished.

---
+++ krogoth_gantry/DVCManager/Experimental/UltraEditorDocument/Services/AKClassEditorComponent.js
@@ -0,0 +1,48 @@
+(function () {
+    'use strict';
+    angular
+        .module('app.FUSE_APP_NAME')
+        .factory('_DJANGULAR_SERVICE_NAME_', _DJANGULAR_SERVICE_NAME_);
+    /** @ngInject */
+    function _DJANGULAR_SERVICE_NAME_($log, $http, $q) {
+        var service = {
+            loadMasterInitializer: loadMasterInitializer
+        };
+        function loadMasterInitializer(selectedMasterId) {
+            var deferred = $q.defer();
+            var servicesPendingRequest = [];
+            var directivesPendingRequest = [];
+            var slavesPendingRequest = [];
+            var returnedJsonFull = {};
+            $http({
+                method: 'GET',
+                url: '/krogoth_gantry/viewsets/MasterViewController/' + selectedMasterId + '/'
+            }).then(function successCallback(response) {
+                returnedJsonFull = response.data;
+                for (var i = 0; i < response.data.djangular_service.length; i++) {
+                    var item_in = response.data.djangular_service[i];
+                    servicesPendingRequest.push(item_in);
+                }
+                for (var i = 0; i < response.data.djangular_directive.length; i++) {
+                    var item_in = response.data.djangular_directive[i];
+                    directivesPendingRequest.push(item_in);
+                }
+                for (var i = 0; i < response.data.djangular_slave_vc.length; i++) {
+                    var item_in = response.data.djangular_slave_vc[i];
+                    slavesPendingRequest.push(item_in);
+                }
+                var firstCompletedAPICall = {
+                    services: servicesPendingRequest,
+                    directives: directivesPendingRequest,
+                    slaves: slavesPendingRequest,
+                    objectList: returnedJsonFull
+                };
+                deferred.resolve(firstCompletedAPICall);
+            }, function errorCallback(response) {
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        }
+        return service;
+    }
+})();
---
+++ krogoth_gantry/DVCManager/Experimental/UltraEditorDocument/Services/treeDataManipulator.js
@@ -0,0 +1,32 @@
+(function() {
+    'use strict';
+    angular
+        .module('app.FUSE_APP_NAME')
+        .factory('_DJANGULAR_SERVICE_NAME_', _DJANGULAR_SERVICE_NAME_);
+    /** @ngInject */
+    function _DJANGULAR_SERVICE_NAME_($log, $http, $q) {
+        $log.log('Hello. The service _DJANGULAR_SERVICE_NAME_ is online ! ! !');
+
+        var service = {
+            setUpBoilerplateTreeJson: setUpBoilerplateTreeJson
+        };
+
+        function setUpBoilerplateTreeJson() {
+            var deferred = $q.defer();
+            $http({
+                method: 'GET',
+                url: '/krogoth_gantry/MasterViewControllerEditorList/'
+            }).then(function successCallback(response) {
+                /// Success
+                deferred.resolve(response.data);
+            }, function errorCallback(response) {
+                /// Fail
+                deferred.reject(response);
+            });
+            return deferred.promise;
+        }
+
+        return service;
+    }
+})();
+
