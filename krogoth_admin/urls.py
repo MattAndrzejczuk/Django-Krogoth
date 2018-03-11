a62feb4eb4571487bfab9ce1dc5ee3620f5d4ecb
cinicraft <matt@jawn.it>
2018-03-11 18:52:17 -0400

    backend should now be able to rename and create directives and templates

--- krogoth_admin/urls.py
+++ krogoth_admin/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 from krogoth_admin.views import UncommitedSQLViewSet
-from krogoth_admin.views_filesystem import RenameService, CreateService, RenameDirective, RenameTemplate
+from krogoth_admin.views_filesystem import RenameService, CreateService, RenameDirective, \
+    CreateDirective, RenameTemplate, CreateTemplate
 
 
 router = DefaultRouter()
@@ -9,14 +10,15 @@ router.register(r'UncommitedSQL', UncommitedSQLViewSet)
 
 
 
-# from rest_auth.views import (
-#     LoginView, LogoutView, UserDetailsView, PasswordChangeView,
-#     PasswordResetView, PasswordResetConfirmView, RegisterUserBasic
-# )
-
-
 urlpatterns = [
     url(r'^KrogothAdministration/', include(router.urls)),
-    url(r'^renameAngularJSService/$', RenameService.as_view(), name='Rename AngularJS Module'),
+
+    url(r'^renameAngularJSService/$', RenameService.as_view(), name='Rename AngularJS Service'),
     url(r'^createAngularJSService/$', CreateService.as_view(), name='Create AngularJS Service'),
+
+    url(r'^renameAngularJSDirective/$', RenameDirective.as_view(), name='Rename AngularJS Directive'),
+    url(r'^createAngularJSDirective/$', CreateDirective.as_view(), name='Create AngularJS Directive'),
+
+    url(r'^renameAngularJSTemplate/$', RenameTemplate.as_view(), name='Rename AngularJS Template'),
+    url(r'^createAngularJSTemplate/$', CreateTemplate.as_view(), name='Create AngularJS Template'),
 ]
--- krogoth_admin/views_filesystem.py
+++ krogoth_admin/views_filesystem.py
@@ -2,7 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
 from rest_framework.permissions import IsAdminUser
-from krogoth_gantry.models import KrogothGantryService, KrogothGantryMasterViewController
+from krogoth_gantry.models import KrogothGantryService, KrogothGantryMasterViewController, KrogothGantryDirective
+from moho_extractor.models import IncludedHtmlMaster
 from jawn.settings import BASE_DIR
 import os
 
@@ -32,32 +33,23 @@ class RenameService(APIView):
         return Response({"result": "success"}, status=200)
 
 class CreateService(APIView):
-    # permission_classes = (IsAdminUser)
     def post(self, request, format=None):
         path_0 = str(request.data["path_0"])
         path_1 = str(request.data["path_1"])
         index = int(request.data["index"])
-
         master_name = str(request.data["master_name"])
         new_name = str(request.data["new_name"])
-
         sys_path = BASE_DIR + "/krogoth_gantry/DVCManager/"
         sql_path = path_0 + "/" + path_1 + "/" + master_name + "/Services/"
-
-        # old_path = sys_path + sql_path + old_name + ".js"
         new_path = sys_path + sql_path + new_name + ".js"
-
         make_blank = open(new_path, 'w')
         make_blank.write("/* " + new_name + " Service */")
         make_blank.close()
-
         new_sql = KrogothGantryService(name=new_name, title=new_name)
         new_sql.save()
-
         mastervc = KrogothGantryMasterViewController.objects.get(name=master_name)
         mastervc.djangular_service.add(new_sql)
         mastervc.save()
-
         node_for_frontend = {
             "id": new_sql.id,
             "parentIndex": 4,
@@ -77,12 +69,8 @@ class CreateService(APIView):
             "hasUnsavedChanges": False,
             "icon": "language-javascript"
         }
-
         return Response(node_for_frontend, status=201)
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-
-
 
 
 # Directives
@@ -92,26 +80,59 @@ class RenameDirective(APIView):
         path_0 = str(request.data["path_0"])
         path_1 = str(request.data["path_1"])
         path_2 = str(request.data["path_2"])
-
         old_name = str(request.data["old_name"])
         new_name = str(request.data["new_name"])
-
         sys_path = BASE_DIR + "/krogoth_gantry/DVCManager/"
         sql_path = path_0 + "/" + path_1 + "/" + path_2 + "/Directives/"
-
         old_path = sys_path + sql_path + old_name + ".js"
         new_path = sys_path + sql_path + new_name + ".js"
-
         os.rename(old_path, new_path)
-        renamed_sql = KrogothGantryService.objects.get(name=old_name)
+        renamed_sql = KrogothGantryDirective.objects.get(name=old_name)
         renamed_sql.name = new_name
         renamed_sql.title = new_name
         renamed_sql.save()
         return Response({"result": "success"}, status=200)
+
+class CreateDirective(APIView):
+    # permission_classes = (IsAdminUser)
+    def post(self, request, format=None):
+        path_0 = str(request.data["path_0"])
+        path_1 = str(request.data["path_1"])
+        index = int(request.data["index"])
+        master_name = str(request.data["master_name"])
+        new_name = str(request.data["new_name"])
+        sys_path = BASE_DIR + "/krogoth_gantry/DVCManager/"
+        sql_path = path_0 + "/" + path_1 + "/" + master_name + "/Directives/"
+        new_path = sys_path + sql_path + new_name + ".js"
+        make_blank = open(new_path, 'w')
+        make_blank.write("/* " + new_name + " Directive */")
+        make_blank.close()
+        new_sql = KrogothGantryDirective(name=new_name, title=new_name)
+        new_sql.save()
+        mastervc = KrogothGantryMasterViewController.objects.get(name=master_name)
+        mastervc.djangular_directive.add(new_sql)
+        mastervc.save()
+        node_for_frontend = {
+            "id": new_sql.id,
+            "parentIndex": 3,
+            "index": index,
+            "name": new_sql.name,
+            "title": new_sql.title,
+            "class": "Directive",
+            "nodes": 0,
+            "canRemove": True,
+            "canEdit": True,
+            "isMaster": False,
+            "sourceCode": new_sql.directive_js,
+            "sourceKey": "directive_js",
+            "RESTfulId": new_sql.id,
+            "RESTfulURI": "/krogoth_gantry/viewsets/Directive/" + str(new_sql.id) + "/",
+            "syntax": "javascript",
+            "hasUnsavedChanges": False,
+            "icon": "language-javascript"
+        }
+        return Response(node_for_frontend, status=201)
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-
-
 
 
 # Templates
@@ -121,20 +142,55 @@ class RenameTemplate(APIView):
         path_0 = str(request.data["path_0"])
         path_1 = str(request.data["path_1"])
         path_2 = str(request.data["path_2"])
-
         old_name = str(request.data["old_name"])
         new_name = str(request.data["new_name"])
-
         sys_path = BASE_DIR + "/krogoth_gantry/DVCManager/"
         sql_path = path_0 + "/" + path_1 + "/" + path_2 + "/partialsHTML/"
-
         old_path = sys_path + sql_path + old_name + ".js"
         new_path = sys_path + sql_path + new_name + ".js"
-
         os.rename(old_path, new_path)
-        renamed_sql = KrogothGantryService.objects.get(name=old_name)
+        renamed_sql = IncludedHtmlMaster.objects.get(name=old_name)
         renamed_sql.name = new_name
         renamed_sql.title = new_name
         renamed_sql.save()
         return Response({"result": "success"}, status=200)
+
+class CreateTemplate(APIView):
+    def post(self, request, format=None):
+        path_0 = str(request.data["path_0"])
+        path_1 = str(request.data["path_1"])
+        index = int(request.data["index"])
+        master_name = str(request.data["master_name"])
+        new_name = str(request.data["new_name"])
+        sys_path = BASE_DIR + "/krogoth_gantry/DVCManager/"
+        sql_path = path_0 + "/" + path_1 + "/" + master_name + "/partialsHTML/"
+        new_path = sys_path + sql_path + new_name + ".js"
+        new_sql = IncludedHtmlMaster(name=new_name, title=new_name)
+        new_sql.save()
+        make_blank = open(new_path, 'w')
+        make_blank.write("/* " + new_name + " IncludedHtmlMaster */\n\n" + new_sql.contents)
+        make_blank.close()
+        mastervc = KrogothGantryMasterViewController.objects.get(name=master_name)
+        mastervc.partial_html.add(new_sql)
+        mastervc.save()
+        node_for_frontend = {
+            "id": new_sql.id,
+            "parentIndex": 5,
+            "index": index,
+            "name": new_sql.name,
+            "title": new_sql.name,
+            "class": "NgIncludedHtml",
+            "nodes": 0,
+            "canRemove": True,
+            "canEdit": True,
+            "isMaster": False,
+            "sourceCode": new_sql.contents,
+            "sourceKey": "contents",
+            "RESTfulId": new_sql.id,
+            "RESTfulURI": "/krogoth_gantry/viewsets/IncludedHtmlMaster/" + str(new_sql.id) + "/",
+            "syntax": "javascript",
+            "hasUnsavedChanges": False,
+            "icon": "language-javascript"
+        }
+        return Response(node_for_frontend, status=201)
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
