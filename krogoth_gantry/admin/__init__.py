6306436f0bcd3d08ff0d5900f1b25228d0689ee7
MattAndrzejczuk <cinicraft@me.com>
2020-11-28 20:15:30 -0500

    finally fixed django admin panel, got rid of bad opaque HTML style tags.

---
+++ krogoth_gantry/admin/__init__.py
@@ -0,0 +1,10 @@
+# extras
+from krogoth_gantry.admin.admin_examples import *
+from krogoth_gantry.admin.admin_chat import *
+from krogoth_gantry.admin.admin_forums import *
+
+# krogoth framework
+from krogoth_gantry.admin.admin_akfoundation import *
+from krogoth_gantry.admin.admin_akthemes import *
+from krogoth_gantry.admin.admin_manager import *
+from krogoth_gantry.admin.admin_mvc import *
