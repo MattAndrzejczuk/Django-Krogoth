31d1183e9b72d7bf44933f2595d4e825baa5922f
Matt A <cinicraft@me.com>
2020-12-18 13:28:32 -0500

    uwsgi issue

--- jawn/wsgi_django.py
+++ jawn/wsgi_django.py
@@ -4,9 +4,12 @@ import uwsgi
 from uwsgidecorators import timer
 from django.utils import autoreload
 
-@timer(1)
-def change_code_gracefull_reload(sig):
-    if autoreload.code_changed():
-        uwsgi.reload()
+
+# @timer(1)
+# def change_code_gracefull_reload(sig):
+    # if autoreload.file_changed():
+    # if autoreload.code_changed():
+    #     uwsgi.reload()
+
 
 application = get_wsgi_application()
