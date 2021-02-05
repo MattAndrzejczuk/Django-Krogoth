a39c5890feb84cb277c262445b86e13cdab89013
MattAndrzejczuk <cinicraft@me.com>
2021-02-05 16:30:11 -0500

    Restored token authentication in settings.

--- jawn/settings.py
+++ jawn/settings.py
@@ -59,9 +59,9 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.BasicAuthentication',
-        # 'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
     # 'DEFAULT_THROTTLE_CLASSES': (
